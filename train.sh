253d64426d7b4683827117ac0f30fdb8dab87cc0
uzair789 <uzaira@andrew.cmu.edu>
2021-04-06 00:04:03 -0400

    [ultron] resnet50_binary

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-EXP_NAME='resnet18_binary'
+EXP_NAME='resnet50_binary'
 
-python train.py --dataset coco --coco_path /media/School/Datasets/coco --depth 18 --exp_name ${EXP_NAME} | tee ${EXP_NAME}/log.txt
+python train.py --dataset coco --coco_path /media/School/Datasets/coco --depth 50 --exp_name ${EXP_NAME} | tee ${EXP_NAME}/log.txt
 
