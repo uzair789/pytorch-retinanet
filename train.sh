ef1f1cd0c4be2b7eda039dc67c7ac3f611b7691d
uzair789 <uzaira@andrew.cmu.edu>
2021-05-24 00:45:38 -0400

    [ultron] Reduced lr to 0.0001 for the binary network

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ export CUDA_VISIBLE_DEVICES='0,1,2,3'
 OUTPUT_FOLDER='./results'
 EXP_NAME='resnet18_backbone_binary'
 
-python train.py --dataset coco --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.01 --batch_size 8 --epochs 12
+python train.py --dataset coco --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12
 
