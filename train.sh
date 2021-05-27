5b6dc0d0ae77396e42ae0bdd3082beaa7092b4e3
uzair789 <uzaira@andrew.cmu.edu>
2021-05-27 02:32:46 -0400

    [ultron] Trainign the studnet for 24 epochs to rule out the train the student for longet argument. I loaded the trained student model during one of the KD experiments and it is possible that instead of KD helping, the longer epoch size of 24 is helping the model. To make sure KD is helping, I am training a student with 24 epochs to compare performance at the end of KD experiment with 12 epochs

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES='0,1,2,3'
 
 OUTPUT_FOLDER='./results'
-EXP_NAME='resnet18_backbone_binary'
+EXP_NAME='resnet18_backbone_binary_24Epochs'
 
-python train.py --dataset coco --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12
+python train.py --dataset coco --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 24
 
