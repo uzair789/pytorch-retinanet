21d81b3d3ba8b0f64e4f334c0434d91661a0faa2
uzair789 <uzaira@andrew.cmu.edu>
2021-06-02 15:44:18 -0400

    [ultron] Training with  fdc = 0.005 and cdc and rdc = 1

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES='4,5'
+export CUDA_VISIBLE_DEVICES='6,7'
 
 OUTPUT_FOLDER='./results'
-FDC=0.0001
+FDC=0.005
 EXP_NAME="resnet18_backbone_binary_distillation_head_plus_featuresWithCoeff${FDC}"
 
 python train.py --dataset coco --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 --fdc ${FDC}
