9adce73b1e82cbdefd418f1e0dcd4e4b6d3e3121
uzair789 <uzaira@andrew.cmu.edu>
2021-06-15 00:15:11 -0400

    [ultron] reduced 4 cards to 2 cards for positives sum

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES='2,3,4,5'
+export CUDA_VISIBLE_DEVICES='2,3'
 
 OUTPUT_FOLDER='./results'
 FDC=0
 #EXP_NAME="dummy_resnet18_backbone_binary_distillation_head_plus_featuresWithCoeff${FDC}"
-EXP_NAME="resnet18_backbone_binary_distillation_head_on_positive_indices_sumloss_4cards"
+EXP_NAME="resnet18_backbone_binary_distillation_head_on_positive_indices_sumloss_2cards"
 
 python train.py --dataset coco --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 --fdc ${FDC}
 
