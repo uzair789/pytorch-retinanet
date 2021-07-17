0146283a3c9516435adb4348d7c794eee96c3d87
uzair789 <uzair.789@hotmail.coom>
2021-07-17 13:09:03 -0400

    [sierra] runs crashed with some weird json error on layer12 and layer123 models. REstartging thse runs on this commit

--- train_sierra.sh
+++ train_sierra.sh
@@ -4,9 +4,10 @@ OUTPUT_FOLDER='./results'
 #FDC=(1 0.1 0.01)
 LR=0.0001
 batch_size=8
-i=0
+i=2
 #NET_TYPE=('layer4_binary' 'layer43_binary' 'layer432_binary')
-NET_TYPE=('layer1_binary' 'layer12_binary' 'layer123_binary')
+#NET_TYPE=('layer1_binary' 'layer12_binary' 'layer123_binary')
+NET_TYPE=('layer12_binary' 'layer123_binary')
 # for batch_size in ${BATCH_SIZES[@]}; do
 
 for net_type in ${NET_TYPE[@]}; do
