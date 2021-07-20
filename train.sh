f9eea6ba961caab36c1379a1f9d6a8574696e1c7
uzair789 <uzaira@andrew.cmu.edu>
2021-07-20 03:41:06 -0400

    [ultron] progressive distillation, now distilling layer12 (teacher) over to layer123(student) without binarization on the downsample layer

--- train.sh
+++ train.sh
@@ -5,7 +5,8 @@ CDC=1
 RDC=1
 FDC=0
 #EXP_NAME="dummy_resnet18_backbone_binary_distillation_head_plus_featuresWithCoeff${FDC}"
-EXP_NAME="resnet18_binary_backbone_distillation_head_teacher_layer1_cdc${CDC}_rdc${RDC}_fdc${FDC}"
+#EXP_NAME="resnet18_binary_backbone_distillation_head_teacher_layer1_cdc${CDC}_rdc${RDC}_fdc${FDC}"
+EXP_NAME="resnet18_layer123_binary_backbone_distillation_head_teacher_layer12_cdc${CDC}_rdc${RDC}_fdc${FDC}"
 
 python train.py --dataset coco --caption progressive_distillation --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC}
 
