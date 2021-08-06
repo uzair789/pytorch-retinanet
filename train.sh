eab71e7ca678b6c047e09101ab17ef701db098b1
uzair789 <uzaira@andrew.cmu.edu>
2021-08-05 21:40:46 -0400

    [ultron] Training BiRealnet18 backbone without the Imagenet pretrained model

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES='4,5'
+export CUDA_VISIBLE_DEVICES='6,7'
 
 OUTPUT_FOLDER='./results'
-EXP_NAME='BiRealNet18_backbone_binary_Imagenet_pretrained_backbone'
+EXP_NAME='BiRealNet18_backbone_binary_from_scratch'
 
-python train.py --dataset coco --arch 'BiRealNet18' --pretrain --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12
+python train.py --dataset coco --arch 'BiRealNet18' --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 #--pretrain
 
