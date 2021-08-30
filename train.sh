bd4c65897426799519b8da8ba931836d951f2e25
uzair789 <uzaira@andrew.cmu.edu>
2021-08-30 00:44:27 -0400

    [ultron] separated the classification, Regression and PyramidFeatures modules for the birealnet by adding them to the birealnet.py file. Earlier I was importing them from the model.py.

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES='4,5'
+export CUDA_VISIBLE_DEVICES='6,7'
 
 OUTPUT_FOLDER='./results'
 CDC=1
@@ -7,7 +7,7 @@ FDC=0
 #EXP_NAME="dummy_resnet18_backbone_binary_distillation_head_plus_featuresWithCoeff${FDC}"
 #EXP_NAME="resnet18_binary_backbone_distillation_head_teacher_layer1_cdc${CDC}_rdc${RDC}_fdc${FDC}"
 #EXP_NAME="resnet18_binary_backbone_distillation_head_teacher_layer123_cdc${CDC}_rdc${RDC}_fdc${FDC}"
-EXP_NAME="reproduce_rerun_commit_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_distillation_head"
+EXP_NAME="seprate_CLassRegressPyramid_for_Bireal_reproduce_rerun_commit_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_distillation_head"
 CAPTION='BiReal18_distillation'
 python train.py --dataset coco --caption ${CAPTION} --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC}
 
