08c2d8e3b256aec34c41e8394077aae2249b3906
uzair789 <uzaira@andrew.cmu.edu>
2021-08-30 16:39:08 -0400

    [ultron] Added the binary forward into the CLss and REg blocks in the birealnet.py file. Not using the skip connections

--- retinanet/birealnet.py
+++ retinanet/birealnet.py
@@ -86,35 +86,41 @@ class PyramidFeatures(nn.Module):
 
 
 class RegressionModel(nn.Module):
-    def __init__(self, num_features_in, num_anchors=9, feature_size=256):
+    def __init__(self, num_features_in, num_anchors=9, feature_size=256, is_bin=False):
         super(RegressionModel, self).__init__()
 
+        # not binarizing because treating this as the first layer dealing with input
         self.conv1 = conv2d(num_features_in, feature_size, kernel_size=3, padding=1)
-        self.act1 = activation()
+        # self.act1 = activation()
 
-        self.conv2 = conv2d(feature_size, feature_size, kernel_size=3, padding=1)
-        self.act2 = activation()
+        self.conv2 = conv2d(feature_size, feature_size, kernel_size=3, padding=1, is_bin=is_bin)
+        self.act2 = activation(is_bin=is_bin)
 
-        self.conv3 = conv2d(feature_size, feature_size, kernel_size=3, padding=1)
-        self.act3 = activation()
+        self.conv3 = conv2d(feature_size, feature_size, kernel_size=3, padding=1, is_bin=is_bin)
+        self.act3 = activation(is_bin=is_bin)
 
-        self.conv4 = conv2d(feature_size, feature_size, kernel_size=3, padding=1)
-        self.act4 = activation()
+        self.conv4 = conv2d(feature_size, feature_size, kernel_size=3, padding=1, is_bin=is_bin)
+        self.act4 = activation(is_bin=is_bin)
 
         self.output = conv2d(feature_size, num_anchors * 4, kernel_size=3, padding=1)
 
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.act1(out)
 
-        out = self.conv2(out)
-        out = self.act2(out)
+        out1 = self.conv1(x)
+        #out = self.act1(out)
 
-        out = self.conv3(out)
-        out = self.act3(out)
+        out = self.act2(out1)
+        out2 = self.conv2(out) #+ out1
+        #out = self.act2(out)
 
-        out = self.conv4(out)
-        out = self.act4(out)
+
+        out = self.act3(out2)
+        out3 = self.conv3(out) #+ out2
+        #out = self.act3(out)
+
+        out = self.act4(out3)
+        out = self.conv4(out) #+ out3
+        #out = self.act4(out)
 
         out = self.output(out)
 
@@ -125,51 +131,57 @@ class RegressionModel(nn.Module):
 
 
 class ClassificationModel(nn.Module):
-    def __init__(self, num_features_in, num_anchors=9, num_classes=80, prior=0.01, feature_size=256):
-        super(ClassificationModel, self).__init__()
+     def __init__(self, num_features_in, num_anchors=9, num_classes=80, prior=0.01, feature_size=256, is_bin=False):
+         super(ClassificationModel, self).__init__()
 
-        self.num_classes = num_classes
-        self.num_anchors = num_anchors
+         self.num_classes = num_classes
+         self.num_anchors = num_anchors
 
-        self.conv1 = conv2d(num_features_in, feature_size, kernel_size=3, padding=1)
-        self.act1 = activation()
+         self.conv1 = conv2d(num_features_in, feature_size, kernel_size=3, padding=1)
+         #self.act1 = activation()
 
-        self.conv2 = conv2d(feature_size, feature_size, kernel_size=3, padding=1)
-        self.act2 = activation()
+         self.conv2 = conv2d(feature_size, feature_size, kernel_size=3, padding=1, is_bin=is_bin)
+         self.act2 = activation(is_bin=is_bin)
 
-        self.conv3 = conv2d(feature_size, feature_size, kernel_size=3, padding=1)
-        self.act3 = activation()
+         self.conv3 = conv2d(feature_size, feature_size, kernel_size=3, padding=1, is_bin=is_bin)
+         self.act3 = activation(is_bin=is_bin)
 
-        self.conv4 = conv2d(feature_size, feature_size, kernel_size=3, padding=1)
-        self.act4 = activation()
+         self.conv4 = conv2d(feature_size, feature_size, kernel_size=3, padding=1, is_bin=is_bin)
+         self.act4 = activation(is_bin=is_bin)
 
-        self.output = conv2d(feature_size, num_anchors * num_classes, kernel_size=3, padding=1)
-        self.output_act = nn.Sigmoid()
+         self.output = conv2d(feature_size, num_anchors * num_classes, kernel_size=3, padding=1)
+         self.output_act = nn.Sigmoid()
 
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.act1(out)
+     def forward(self, x):
+         out1 = self.conv1(x)
+         #out = self.act1(out)
 
-        out = self.conv2(out)
-        out = self.act2(out)
+         out = self.act2(out1)
+         out2 = self.conv2(out) #+ out1
+         #out = self.act2(out)
 
-        out = self.conv3(out)
-        out = self.act3(out)
+         out = self.act3(out2)
+         out3 = self.conv3(out) #+ out2
+         #out = self.act3(out)
 
-        out = self.conv4(out)
-        out = self.act4(out)
+         out = self.act4(out3)
+         out = self.conv4(out) #+ out3
+         #out = self.act4(out)
 
-        out = self.output(out)
-        out = self.output_act(out)
+         out = self.output(out)
+         out = self.output_act(out)
 
-        # out is B x C x W x H, with C = n_classes + n_anchors
-        out1 = out.permute(0, 2, 3, 1)
+         # out is B x C x W x H, with C = n_classes + n_anchors
+         out1 = out.permute(0, 2, 3, 1)
 
-        batch_size, width, height, channels = out1.shape
+         batch_size, width, height, channels = out1.shape
 
-        out2 = out1.view(batch_size, width, height, self.num_anchors, self.num_classes)
+         out2 = out1.view(batch_size, width, height, self.num_anchors, self.num_classes)
 
-        return out2.contiguous().view(x.shape[0], -1, self.num_classes)
+         return out2.contiguous().view(x.shape[0], -1, self.num_classes)
+
+
+
 
 class BinaryActivation(nn.Module):
     def __init__(self):
