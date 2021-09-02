d4bd005fccdcdb909cde51bc226ce16ea1b2c2fd
uzair789 <uzaira@andrew.cmu.edu>
2021-09-02 01:31:15 -0400

    [ultron] Training the mdoel with heads binarized with skip connections with LambdaLR scheduler to compare with the se3 model with LambdaLR scheduler to understand the importance of the attention models

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES='0,1'
 OUTPUT_FOLDER='./results'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_binary_Imagenet_pretrain'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch'
-EXP_NAME='BiRealNet18_backbone_plus_heads_shortcuts_Imagenet_pretrain'
+EXP_NAME='BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR'
 
-python train.py --server 'Sierra' --dataset coco --arch 'BiRealNet18' --coco_path /media/apple/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 --pretrain
+python train.py --dataset coco --arch 'BiRealNet18' --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 #--pretrain
 
