85361b02a71740443e27c5aefd8ea98deaad1813
uzair789 <uzaira@andrew.cmu.edu>
2021-09-02 22:00:18 -0400

    [ultron] Trying the distillation on the head with shortcuts experiment with LambdaLR shceduler during distillation but the student was pre trained with oldScheduler. Tryung to see if LambdaLR boosts the distillation performance

--- train.py
+++ train.py
@@ -168,7 +168,8 @@ def main(args=None):
 
     optimizer = optim.Adam(retinanet.parameters(), lr=parser.lr)
 
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
+    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lambda step : (1.0-step/parser.epochs), last_epoch=-1)
+    #scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
 
     loss_hist = collections.deque(maxlen=500)
 
@@ -327,7 +328,8 @@ def main(args=None):
 
             mAP = csv_eval.evaluate(dataset_val, retinanet)
 
-        scheduler.step(np.mean(epoch_loss))
+        #scheduler.step(np.mean(epoch_loss))
+        scheduler.step()
 
         torch.save(retinanet.module, os.path.join(output_folder_path, '{}_retinanet_{}.pt'.format(parser.dataset, epoch_num)))
 
