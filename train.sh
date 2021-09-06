724d948d03615896dd8df94afcd0593a47458b75
uzair789 <uzaira@andrew.cmu.edu>
2021-09-06 11:24:39 -0400

    [ultron] Training with distillation LambdaLR lr=0.0001 on the previous chain of experiments. Training with old scheduler and 0.0001 shows poor convergence. THis might be better, lets see

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES='2,3'
+export CUDA_VISIBLE_DEVICES='4,5'
 
 OUTPUT_FOLDER='./results'
 CDC=1
@@ -9,7 +9,7 @@ LR=0.0001
 #EXP_NAME="dummy_resnet18_backbone_binary_distillation_head_plus_featuresWithCoeff${FDC}"
 #EXP_NAME="resnet18_binary_backbone_distillation_head_teacher_layer1_cdc${CDC}_rdc${RDC}_fdc${FDC}"
 #EXP_NAME="resnet18_binary_backbone_distillation_head_teacher_layer123_cdc${CDC}_rdc${RDC}_fdc${FDC}"
-EXP_NAME="BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_distillation_head_oldScheduler_lr${LR}"
+EXP_NAME="BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_distillation_head_LambdaLR_lr${LR}"
 CAPTION='BiReal18_distillation'
-python train.py --dataset coco --lrScheduler Old --caption ${CAPTION} --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr ${LR} --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC}
+python train.py --dataset coco --lrScheduler LambdaLR --caption ${CAPTION} --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr ${LR} --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC}
 
