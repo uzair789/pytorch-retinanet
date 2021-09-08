cb3d9380b1a28871ca10e8c3ea609183ea443013
uzair789 <uzair.789@hotmail.coom>
2021-09-08 02:28:02 -0400

    [sierra] restarting the previous commit as it crashed because of the JSON error

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES='4,5'
+export CUDA_VISIBLE_DEVICES='2,4'
 
 OUTPUT_FOLDER='./results'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_binary_Imagenet_pretrain'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch'
-EXP_NAME='BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN'
+EXP_NAME='rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN'
 
 python train.py --server Sierra --dataset coco --arch 'BiRealNet18' --coco_path /media/apple/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 #--pretrain
 
