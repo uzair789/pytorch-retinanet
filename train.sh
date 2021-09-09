e4fcb686b7883b2577759159b6aed20a80ed1464
uzair789 <uzaira@andrew.cmu.edu>
2021-09-08 23:19:18 -0400

    [ultron] Trying distillation on the full binarized model (backbone + heads + FPN). Loaded baseline here is the FPN binarized with 1x1 stride 2 without avgpooling BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN. THe teacher is he full precision model. Training with Lambda LR starting with 0.0001

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES='4,5'
+export CUDA_VISIBLE_DEVICES='0,1'
 
 OUTPUT_FOLDER='./results'
 CDC=1
@@ -9,7 +9,8 @@ LR=0.0001
 #EXP_NAME="dummy_resnet18_backbone_binary_distillation_head_plus_featuresWithCoeff${FDC}"
 #EXP_NAME="resnet18_binary_backbone_distillation_head_teacher_layer1_cdc${CDC}_rdc${RDC}_fdc${FDC}"
 #EXP_NAME="resnet18_binary_backbone_distillation_head_teacher_layer123_cdc${CDC}_rdc${RDC}_fdc${FDC}"
-EXP_NAME="BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_distillation_head_LambdaLR_lr${LR}"
+#EXP_NAME="BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_distillation_head_LambdaLR_lr${LR}"
+EXP_NAME="BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN_distillation_head_LambdaLR_lr${LR}"
 CAPTION='BiReal18_distillation'
 python train.py --dataset coco --lrScheduler LambdaLR --caption ${CAPTION} --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr ${LR} --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC}
 
