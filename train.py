7d4b3eba89efbf3c6db2c2a704906e124cf4d70a
uzair789 <uzaira@andrew.cmu.edu>
2021-09-14 18:52:50 -0400

    [ultron] Epoch 7 checkpoint for the full precision teacher when doing the changing teacher every epoch experiment showed some signs of convergence. Lets see if this improves the model after distillation

--- train.py
+++ train.py
@@ -125,11 +125,11 @@ def main(args=None):
         #model_folder = 'BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch'
         #model_folder = 'BiRealNet18_backbone_plus_SE_attention_3_heads_with_shortcuts_LambdaLR'
         #model_folder = 'BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR'
-        ###model_folder = 'BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN'
+        model_folder = 'BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN'
         # retinanet = model.resnet18(num_classes=dataset_train.num_classes(), pretrained=True, is_bin=True)
         #retinanet = torch.load('results/resnet18_layer123_binary_backbone_binary/coco_retinanet_11.pt')
-        ###retinanet = torch.load('results/{}/coco_retinanet_11.pt'.format(model_folder))
-        retinanet = birealnet18(checkpoint_path=None, num_classes=dataset_train.num_classes())
+        retinanet = torch.load('results/{}/coco_retinanet_11.pt'.format(model_folder))
+        #retinanet = birealnet18(checkpoint_path=None, num_classes=dataset_train.num_classes())
         #retinanet.load_state_dict(checkpoint)
         print('student loaded!')
         print(retinanet)
@@ -140,7 +140,8 @@ def main(args=None):
             #                                   is_bin=False)
             #retinanet_teacher = torch.load('results/resnet18_layer1_binary_backbone_binary/coco_retinanet_11.pt')
             #retinanet_teacher = torch.load('results/resnet18_layer123_binary_backbone_distillation_head_teacher_layer12_cdc1_rdc1_fdc0/coco_retinanet_11.pt')
-            retinanet_teacher = torch.load('results/resnet18_backbone_full_precision/coco_retinanet_11.pt')
+            #retinanet_teacher = torch.load('results/resnet18_backbone_full_precision/coco_retinanet_11.pt')
+            retinanet_teacher = torch.load('results/resnet18_backbone_full_precision/coco_retinanet_7.pt')
             # retinanet_teacher.load_state_dict(checkpoint_teacher)
             print('teacher loaded!')
             print(retinanet_teacher)
--- train.sh
+++ train.sh
@@ -11,7 +11,7 @@ LR=0.0001
 #EXP_NAME="resnet18_binary_backbone_distillation_head_teacher_layer123_cdc${CDC}_rdc${RDC}_fdc${FDC}"
 #EXP_NAME="BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_distillation_head_LambdaLR_lr${LR}"
 #EXP_NAME="BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN_distillation_head_LambdaLR_lr${LR}"
-EXP_NAME="from_scratch_distillation_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_binary_FPN_distillation_head_LambdaLR_lr${LR}"
+EXP_NAME="teacher_checkpoint7_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_binary_FPN_distillation_head_LambdaLR_lr${LR}"
 CAPTION='BiReal18_distillation'
 python train.py --dataset coco --lrScheduler LambdaLR --caption ${CAPTION} --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr ${LR} --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC}
 
