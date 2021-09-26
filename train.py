41a8cfda92696f3c84e8483fd1f75ce75c98fe4a
uzair789 <uzaira@andrew.cmu.edu>
2021-09-26 05:12:14 -0400

    [ultron] Trying training student with distillation from scratch with changing teacher

--- train.py
+++ train.py
@@ -130,12 +130,12 @@ def main(args=None):
         #model_folder = 'BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch'
         #model_folder = 'BiRealNet18_backbone_plus_SE_attention_3_heads_with_shortcuts_LambdaLR'
         #model_folder = 'BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR'
-        model_folder = 'BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN'
+        ##model_folder = 'BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN'
         #model_folder = 'rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN'
         # retinanet = model.resnet18(num_classes=dataset_train.num_classes(), pretrained=True, is_bin=True)
         #retinanet = torch.load('results/resnet18_layer123_binary_backbone_binary/coco_retinanet_11.pt')
-        retinanet = torch.load('results/{}/coco_retinanet_11.pt'.format(model_folder))
-        #retinanet = birealnet18(checkpoint_path=None, num_classes=dataset_train.num_classes())
+        ##retinanet = torch.load('results/{}/coco_retinanet_11.pt'.format(model_folder))
+        retinanet = birealnet18(checkpoint_path=None, num_classes=dataset_train.num_classes())
         #retinanet.load_state_dict(checkpoint)
         print('student loaded!')
         print(retinanet)
