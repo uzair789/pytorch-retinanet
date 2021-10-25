49affbd0da2eb2da9d4041eeff8110e36fc56a65
uzair789 <uzaira@andrew.cmu.edu>
2021-10-25 10:32:48 -0400

    [ultron] Runnign epoch4 initialization now as this gave 17 ap1 results. Runnign these runs to check the importance of classification distillation vs regression distillation. On this run, I have set RDC=0 and CDC=8

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES='4,5'
 
 OUTPUT_FOLDER='./results'
 CDC=8
-RDC=8
+RDC=0
 FDC=0
 CLC=1
 RLC=1
@@ -15,7 +15,7 @@ LR=0.0001
 #EXP_NAME="BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN_distillation_head_LambdaLR_lr${LR}"
 #EXP_NAME="normalized_logit_map_teacher_changed_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
 #EXP_NAME="from_scratch_normalized_logit_map_teacher_changed_every_epoch_consistent_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
-EXP_NAME="normalized_logit_map_epoch5_initialization_LambdaLRModel_teacher_changed_every_epoch_consistent_rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
+EXP_NAME="normalized_logit_map_epoch4_initialization_LambdaLRModel_teacher_changed_every_epoch_consistent_rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
 CAPTION='BiReal18_distillation'
 python train.py --dataset coco --lrScheduler LambdaLR --caption ${CAPTION} --coco_path /media/ramdisk/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr ${LR} --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC} --clc ${CLC} --rlc ${RLC}
 
