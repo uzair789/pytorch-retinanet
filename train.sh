30541d04d63fcb44e6b3cb9bafc64ca600201638
uzair789 <uzaira@andrew.cmu.edu>
2021-12-19 21:45:46 -0500

    [ultron ]debug for batchnorm unfreeze. THe other teacher and student I trained with batchnorm unfreeze show very bad results on distillation whether i freeze or unfreeze batchnorm. Student performance is similar with batchnorm unfreeze but I dont know why distillation performance is bad. In this run, I am distilling with the old student and teacher i trained while freezing the batchnorm layers but while distillation i unfreeze the batchnorm layers to see if this changes anything.

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES='0,1'
 
-OUTPUT_FOLDER='./results'
+OUTPUT_FOLDER='./results2'
 CDC=8
 RDC=8
 FDC=0
@@ -15,7 +15,7 @@ LR=0.0001
 #EXP_NAME="BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN_distillation_head_LambdaLR_lr${LR}"
 #EXP_NAME="normalized_logit_map_teacher_changed_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
 #EXP_NAME="from_scratch_normalized_logit_map_teacher_changed_every_epoch_consistent_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
-EXP_NAME="normalized_logit_map_epoch4_initialization_LambdaLRModel_teacher_changed_every_epoch_consistent_rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
+EXP_NAME="bn_unfreeze_normalizedLogitMapEpoch4nitialization_LambdaLRModel_teacher_changed_every_epoch_consistent_rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
 CAPTION='BiReal18_distillation'
 python train.py --dataset coco --lrScheduler LambdaLR --caption ${CAPTION} --coco_path /media/ramdisk/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr ${LR} --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC} --clc ${CLC} --rlc ${RLC}
 
