0f6ad33df6fae9a71d922f150db71b48eeb3459e
uzair789 <uzaira@andrew.cmu.edu>
2021-12-30 02:52:21 -0500

    [ultron] batchnorm freeze is False on the previous commit

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
-export CUDA_VISIBLE_DEVICES='4,5'
+export CUDA_VISIBLE_DEVICES='6,7'
 
 OUTPUT_FOLDER='./results2'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_binary_Imagenet_pretrain'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch'
 #EXP_NAME='rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN(DIS-375)_batchnorm_freeze_False'
-EXP_NAME='rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN(DIS-375)_batchnorm_freeze_True_load_same_binary_units'
+EXP_NAME='rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN(DIS-375)_batchnorm_freeze_False_load_same_binary_units'
 
-python train.py --server Sierra --dataset coco --arch 'BiRealNet18' --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 --freeze_batchnorm #--pretrain
+python train.py --server Sierra --dataset coco --arch 'BiRealNet18' --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 #--freeze_batchnorm #--pretrain
 
