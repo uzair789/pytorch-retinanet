d31727ec7d44bc79cf331f86dbe0f6b9ab866e6e
uzair789 <uzaira@andrew.cmu.edu>
2022-01-17 22:55:43 -0500

    [ultron] Resnet34 full prrecision pretrain True with freeze_batchnorm as False

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
-export CUDA_VISIBLE_DEVICES='6,7'
+export CUDA_VISIBLE_DEVICES='0,1'
 
 OUTPUT_FOLDER='./results2'
 #EXP_NAME='BiRealNet18_backbone_binary_from_scratch'
-DEPTH=18
-EXP_NAME="Resnet${DEPTH}_backbone_full_precision_pretrain_True_freezebatchnorm_True"
+DEPTH=34
+EXP_NAME="Resnet${DEPTH}_backbone_full_precision_pretrain_True_freezebatchnorm_False"
 ARCH='Resnet'
 #ARCH='BiRealNet18'
 
-python train.py --dataset coco --arch ${ARCH} --coco_path /media/School/Datasets/coco --depth $DEPTH --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 --pretrain --freeze_batchnorm
+python train.py --dataset coco --arch ${ARCH} --coco_path /media/School/Datasets/coco --depth $DEPTH --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 --pretrain #--freeze_batchnorm
 
