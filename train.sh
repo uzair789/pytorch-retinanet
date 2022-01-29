8d33818200bc6b1439af309971e3c7e0b41bf5e8
uzair789 <uzaira@andrew.cmu.edu>
2022-01-29 09:53:37 -0500

    [ultron] Training BiRealNet10

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ OUTPUT_FOLDER='./results2'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_binary_Imagenet_pretrain'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch'
 #EXP_NAME='rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN(DIS-375)_batchnorm_freeze_False'
-EXP_NAME='BiRealNet34_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_batchnorm_freeze_False_load_same_binary_units'
+EXP_NAME='BiRealNet10_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_batchnorm_freeze_False_load_same_binary_units'
 
-python train.py --server ultron --dataset coco --arch 'BiRealNet34' --coco_path /media/School/Datasets/coco --depth 34 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 #--freeze_batchnorm #--pretrain
+python train.py --server ultron --dataset coco --arch 'BiRealNet10' --coco_path /media/School/Datasets/coco --depth 10 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 #--freeze_batchnorm #--pretrain
 
