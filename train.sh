390e10f5b1b9117721f74524cab69c020da65b68
uzair789 <uzaira@andrew.cmu.edu>
2022-02-06 18:06:34 -0500

    [ultron] Training BiRealnet34 backbone with FPN FP and heads binary without skip. Verufying of model doesnt converge

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
-export CUDA_VISIBLE_DEVICES='2,3'
+export CUDA_VISIBLE_DEVICES='4,5'
 
 OUTPUT_FOLDER='./results2'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_binary_Imagenet_pretrain'
 #EXP_NAME='BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch'
 #EXP_NAME='rerun_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN(DIS-375)_batchnorm_freeze_False'
 #EXP_NAME='BiRealNet10_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_batchnorm_freeze_False_load_same_binary_units'
-EXP_NAME='BiRealNet10_FPN_FP_heads_binary_without_skip'
+EXP_NAME='BiRealNet34_FPN_FP_heads_binary_without_skip'
 
-python train.py --server ultron --dataset coco --arch 'BiRealNet10' --coco_path /media/School/Datasets/coco --depth 10 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 #--freeze_batchnorm #--pretrain
+python train.py --server ultron --dataset coco --arch 'BiRealNet34' --coco_path /media/School/Datasets/coco --depth 34 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr 0.0001 --batch_size 8 --epochs 12 #--freeze_batchnorm #--pretrain
 
