274237ddfea387d4807f66fdfacc3bccbb1c07d5
uzair789 <uzaira@andrew.cmu.edu>
2022-03-05 13:05:52 -0500

    [ultron] Previous commit with oldScheduler

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES='0,1'
+export CUDA_VISIBLE_DEVICES='2,3'
 
 OUTPUT_FOLDER='./results2'
 CDC=8
@@ -16,8 +16,8 @@ LR=0.0001
 #EXP_NAME="normalized_logit_map_teacher_changed_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_LambdaLR_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
 #EXP_NAME="from_scratch_normalized_logit_map_teacher_changed_every_epoch_consistent_BiRealNet18_backbone_plus_heads_shortcuts_binary_from_scratch_OldScheduler_binary_FPN_distillation_head_LambdaLR_lr${LR}_CDC{$CDC}_RDC{$RDC}_CLC{$CLC}_RLC{$RLC}"
 #EXP_NAME="Ablation_BiRealNet18_FFF_teacher_Dis-644_student_Dis-683_RYWYCNNY"
-EXP_NAME="FP_distillation_teacher_Dis-716_resnet34_student_Dis-644_resnet18"
+EXP_NAME="FP_distillation_teacher_Dis-716_resnet34_student_Dis-644_resnet18_oldScheduler"
 #CAPTION='BiReal18_distillation'
 CAPTION='FP_distillation'
-python train.py --dataset coco --arch FP --lrScheduler LambdaLR --caption ${CAPTION} --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr ${LR} --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC} --clc ${CLC} --rlc ${RLC} --warmup --normalization --change_teacher #--freeze_batchnorm
+python train.py --dataset coco --arch FP --lrScheduler OldScheduler --caption ${CAPTION} --coco_path /media/School/Datasets/coco --depth 18 --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr ${LR} --batch_size 8 --epochs 12 --fdc ${FDC} --cdc ${CDC} --rdc ${RDC} --clc ${CLC} --rlc ${RLC} --warmup --normalization --change_teacher #--freeze_batchnorm
 
