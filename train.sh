dd6a05974b72a37aae4ede3fd850b2c8db5a49db
uzair789 <uzaira@andrew.cmu.edu>
2022-05-18 15:38:46 -0400

    [ultron] Default settings for Resnet50 of lr=1e-5, batch size=2, num_epochs=100. Trying to see if this reaches the map of 33 as claimed on the repo

--- train.sh
+++ train.sh
@@ -4,10 +4,11 @@ OUTPUT_FOLDER='./results3'
 #EXP_NAME='BiRealNet18_backbone_binary_from_scratch'
 DEPTH=50
 #ARCH='ofa'
-LR=0.001
-EXP_NAME="debug_LR${LR}_Resnet${DEPTH}_backbone_full_precision_pretrain_False_freezebatchnorm_False"
+LR=1e-5
+BATCH_SIZE=2
+EXP_NAME="debug_LR${LR}_Resnet${DEPTH}_backbone_full_precision_pretrain_True_freezebatchnorm_False"
 ARCH='Resnet'
 #ARCH='BiRealNet18'
 
-python train.py --dataset coco --arch ${ARCH} --coco_path /media/School/Datasets/coco --depth $DEPTH --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr $LR --batch_size 8 --epochs 12 #--pretrain #--freeze_batchnorm
+python train.py --dataset coco --arch ${ARCH} --coco_path /media/School/Datasets/coco --depth $DEPTH --output_folder ${OUTPUT_FOLDER} --exp_name ${EXP_NAME} --lr $LR --batch_size 2 --epochs 100 --pretrain #--freeze_batchnorm
 
